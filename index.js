732508e5239e1f703f0ff3d2ecc5e4fe861ad47e
Beau Nieuwveld <bnieuwveld@sthgrampians.vic.gov.au>
2022-04-14 15:24:00 +1000

    Second

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://test.mosquitto.org')
 
+const express = require('express')
+const app = express()
+
 client.on('message', function(topic, message) {
     console.log(message.toString())
     client.end()
 })
+
+app.get('/', function(req, res) {
+    res.send('Hello world')
+})
+
+app.listen(80)
